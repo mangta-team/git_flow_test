81f9822e00de8647690d1232dfd6cbb323200c2d
alexjime <alexji.dev@gmail.com>
2022-01-01 14:56:15 +0900

    rebase test - 2

--- Test.java
+++ Test.java
@@ -8,9 +8,14 @@ public class Test {
     }
 
     private void init() {
+<<<<<<< HEAD
         this.id = 1;
         System.out.println("끼워넣기");
         this.value = "hello";
+=======
+        this.id = 2;
+        this.value = "hello2";
+>>>>>>> d7c695a (rebase test - 2)
         System.out.println(id);
     }
 }
